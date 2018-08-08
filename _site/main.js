e6dddd82936f1c074e148a006e9ad4db71bbd8f8
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2018-08-08 15:09:44 +0700

    added about and redid layout/container/tears
    - built about page
    - moved tears out of sections
    - revised styling for tears
    - revised styling for section backgrounds
    - added smooth scrolling
    - tweaked emphasized button hover again
    - need to fix extra space at bottom!

--- _site/main.js
+++ _site/main.js
@@ -7,4 +7,15 @@ $(document).ready(function() {
       $('#mobile-menu').toggleClass('mobile-menu-visible')
   });
 
+  //smooth scrolling better because rachel
+  $("a[href^='#']").click(function () {
+    var link = this.hash.substr(1);
+    var section = $('*[id=' + link + ']');
+
+    $('html,body').animate({
+      scrollTop: section.offset().top - 20
+    }, 800);
+    return false;
+  });
+
 });
